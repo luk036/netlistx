5b5afe863d46a68b75fd6c7b8c30d1b4f60d5375
Wai-Shing Luk <luk036@gmail.com>
2026-01-16 13:19:10 +0800

    No more links in order to fix the CI/CD

--- src/netlistx/netlist.py
+++ src/netlistx/netlist.py
@@ -276,8 +276,8 @@ def read_json(filename: str) -> Netlist:
         data = json.load(fr)
 
     # Convert 'links' to 'edges' for NetworkX compatibility
-    if "links" in data and "edges" not in data:
-        data["edges"] = data.pop("links")
+    # if "links" in data and "edges" not in data:
+    #     data["edges"] = data.pop("links")
 
     ugraph = json_graph.node_link_graph(data)
     num_modules = ugraph.graph["num_modules"]
